7b67baaa74d26d1ba085a0a013e78efad28c82d6
Lauren Posey and Stephanie Gurung <lauren@>
2016-05-31 16:44:48 -0700

    add remove meow-bark functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 $(function (){
   $(".clickable").click(function() {
-    $("h1", this).toggle();
-    $("p", this).toggle();
+    $(this).children().toggle();
+    // $("p", this).toggle();
+  });
+
+  $("button#meow").click(function(){
+    $("ul#kitty").prepend("<li>Meow!</li>");
+    $("ul#doggy").prepend("<li>Woof!</li>");
+    $("ul#kitty").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#doggy").children("li").first().click(function(){
+      $(this).remove();
+    });
+  });
+
+  $("button#bark").click(function(){
+    $("ul#doggy").prepend("<li>Woof!</li>");
+    $("ul#kitty").prepend("<li>Meow!</li>");
+    $("ul#kitty").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#doggy").children("li").first().click(function(){
+      $(this).remove();
+    });
   });
 });
